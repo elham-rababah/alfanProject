69641da7411a273f63ba814955217bd23260dc50
Elhamrababah <Elham.a.rababah@gmail.com>
2018-01-25 15:46:23 +0200

    first step from add Heder to the site

---
+++ src/js/components/Heder/Heder.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default class Heder extends React.Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>heeeer is Heder</h1>
+      </div>
+    );
+  }
+}
--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,20 +1,15 @@
 import React from "react";
+import Heder from "./Heder/Heder";
 
 export default class Layout extends React.Component {
   constructor() {
     super();
-    this.state = {
-      title: "Welcome",
-    };
-  }
-
-  changeTitle(title) {
-    this.setState({title});
   }
 
   render() {
     return (
       <div>
+      	<Heder/>
         <h1>heeeer is Layout</h1>
       </div>
     );
