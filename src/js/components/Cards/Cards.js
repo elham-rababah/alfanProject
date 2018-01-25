681b2cff42fc18484ae464be5a8fe7f00c09b832
Elhamrababah <Elham.a.rababah@gmail.com>
2018-01-25 17:54:20 +0200

    first step from card

--- src/js/components/Cards/Cards.js
+++ src/js/components/Cards/Cards.js
@@ -1,4 +1,16 @@
 import React from "react";
+import { Card, ImageHeader, CardBody, CardFooter } from "react-simple-card";
+
+
+const App = () => (
+  <div>
+    <h2>React Simple Card basic layout with image header</h2>
+    
+  </div>
+);
+
+
+
 
 export default class Cards extends React.Component {
   constructor() {
@@ -9,6 +21,32 @@ export default class Cards extends React.Component {
     return (
       <div>
         <h1>heeeer is Cards</h1>
+        <Card>
+      <ImageHeader alt="testAlt" imageSrc="http://via.placeholder.com/600x250?text=Placeholder" />
+      <CardBody>
+        <p>
+          Phasellus eleifend orci sed neque ultricies viverra. Nulla orci nunc,
+          hendrerit sed malesuada a, pharetra sed sem. Vivamus et vestibulum
+          nulla. Curabitur gravida maximus dolor at scelerisque. In dignissim,
+          diam nec vehicula scelerisque, metus purus vulputate ante, ac faucibus
+          dui orci dignissim ipsum. Nullam euismod turpis id scelerisque congue.
+        </p>
+        <p>
+          Donec blandit risus ullamcorper suscipit sagittis. Nam auctor laoreet
+          mauris ut bibendum. Aliquam et sodales justo. Proin aliquam augue in
+          ipsum rutrum, ac tristique tellus tincidunt. Aenean blandit ultrices
+          pretium. Suspendisse in arcu bibendum, egestas lorem a, fringilla
+          purus. Integer hendrerit nibh tincidunt, tempor diam eget, tincidunt
+          tortor. Praesent volutpat odio metus, et sollicitudin dolor
+          ullamcorper ut.
+        </p>
+      </CardBody>
+      <CardFooter>
+        <p>
+          <a href="#123">Nullam vel</a>
+        </p>
+      </CardFooter>
+    </Card>
       </div>
     );
   }
