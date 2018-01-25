15eabe66a533321d1de925e6ff480adc387bd73f
Elhamrababah <Elham.a.rababah@gmail.com>
2018-01-25 14:37:34 +0200

    create the structure of react js project

---
+++ src/js/components/Layout.js
@@ -0,0 +1,10 @@
+import React from "react";
+export default class Layout extends React.Component {
+	render() {
+    return (
+      <div>
+        <h1>EEEEEEEEEEEEEEEEEE</h1>
+      </div>
+      );
+  }
+}
