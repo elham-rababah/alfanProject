926327af3da872157077579a570e77dd16df9efe
Elhamrababah <Elham.a.rababah@gmail.com>
2018-01-28 13:33:06 +0200

    create socialFollwersSlider first step

--- src/js/components/LeftMenu/FirstSection.js
+++ src/js/components/LeftMenu/FirstSection.js
@@ -3,12 +3,42 @@ import React from "react";
 export default class FirstSection extends React.Component {
   constructor() {
     super();
+    this.state = {
+      dataSliderValue:0,
+      dataSliderMin:0,
+      dataSliderMax:5000000,
+      dataSliderStep:100000,
+
+    } 
   }
+  componentDidMount(){
+    var slider = new Slider('#socialFollwersSlider', {});
+    let currentComponent = this;
+    slider.on("slide", function(sliderValue) {
+      currentComponent.setState({dataSliderValue: sliderValue[1]});
+  });
+
+  }
+  //
 
   render() {
     return (
       <div >
-        <h1>heeeer is FirstSection</h1>
+        Total Social Follwers: <br/> 
+         <div>
+        </div>
+        <br/>
+        <input 
+          id="socialFollwersSlider"
+          data-slider-id='dataSliderId'
+          type="text" 
+          class="span2" 
+          value={this.state.dataSliderValue} 
+          data-slider-min={this.state.dataSliderMin} 
+          data-slider-max={this.state.dataSliderMax} 
+          data-slider-step={this.state.dataSliderStep} 
+          data-slider-value="[222,0]"
+          /> 
       </div>
     );
   }
